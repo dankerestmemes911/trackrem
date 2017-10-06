f72f3ed7fbcf365390880c206e76d63ec86e1286
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-06 17:23:52 +0200

    Update track.php
    Just wrote $conn

--- track.php
+++ track.php
@@ -1,1 +1,12 @@
+<?php
+  // BEGINNING OF SETTINGS
+  $mysql_server = "mysql.server.org";
+  $mysql_user = "TheNSA";
+  $mysql_pass = "ILoveTracking";
+  $mysql_db = "TheDatabase";
+  // ENDING OF SETTINGS
 
+  $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass, $mysql_db);
+    
+  $conn->close();
+?>
