51daa68ba4d2ec6c74f69a74335e65200350e0d2
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-06 14:07:31 +0200

    Update index.php
    Create a base for the index page

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-
+<html>
+  <head>
+    <title>TRACKREM - Index</title>
+  </head>
+  <body>
+    <?php
+        // BEGINNING OF SETTINGS
+        $mysql_server = "mysql.server.org";
+        $mysql_user = "iamthensa";
+        $mysql_pass = "StalinDidN0th1ng";
+        // ENDING OF SETTINGS
+    
+        $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass);
+        $sql = "SELECT ip, isp, country, hasxxx, hascp FROM trackrem_iplist";
+        $result = $conn->query($sql);
+    
+        $conn->close();
+    ?>
+  </body>
+</html>
