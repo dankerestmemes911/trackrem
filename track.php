5aa56f8cc91ef79619584c3e01325bb8a49f64f5
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-07 15:27:59 +0200

    Update track.php
    Created basic tracking code.

--- track.php
+++ track.php
@@ -1,12 +1,32 @@
 <?php
   // BEGINNING OF SETTINGS
-  $mysql_server = "mysql.server.org";
-  $mysql_user = "TheNSA";
-  $mysql_pass = "ILoveTracking";
-  $mysql_db = "TheDatabase";
+    $mysql_server = "localhost";
+    $mysql_user = "username";
+    $mysql_pass = "password";
+    $mysql_db = "database";
   // ENDING OF SETTINGS
 
+  if ($_GET['ip']) {
+    $ip = $_GET["ip"];
+  } else {
+    die("Necessary input not given");
+  }
+
+  $timestamp = time();
+
   $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass, $mysql_db);
-    
+  if ($conn->connect_error) {
+    die("MySQL Error: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO trackrem_processrequests (ip, date)
+  VALUES ('" . $ip . "', '" . $timestamp . "')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "success";
+  } else {
+    die("Inserting into database failed");
+  }
+  
   $conn->close();
 ?>
