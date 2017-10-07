9a48b1bd50404d3eb4c3aded8c5c3808062dda6a
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-07 15:29:59 +0200

    Create process.php
    Gets data from the IPs by processing the entries sent by track.php

---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+  // BEGINNING OF SETTINGS
+    $mysql_server = "localhost";
+    $mysql_user = "id1939753_haskremuser";
+    $mysql_pass = "Ga0La1Sw!";
+    $mysql_db = "id1939753_haskremtestdb";
+  // ENDING OF SETTINGS
+  
+  
+?>
