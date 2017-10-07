98024db781e6f5de84d7982da85d28c46a3e5420
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-07 15:31:37 +0200

    Create process.php
    Used for processing IPs given by track.php

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+  // BEGINNING OF SETTINGS
+    $mysql_server = "localhost";
+    $mysql_user = "username";
+    $mysql_pass = "password";
+    $mysql_db = "database";
+  // ENDING OF SETTINGS
+?>
