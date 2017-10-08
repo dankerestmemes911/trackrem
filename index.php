2b013b148e9106f9d4a373d3a9cd4626795afb89
dankerestmemes911 <dankerestmemes911@gmail.com>
2017-10-08 22:07:35 +0200

    Update index.php
    Updated index.php to show data correctly

--- index.php
+++ index.php
@@ -3,44 +3,28 @@
     <title>TRACKREM - Index</title>
   </head>
   <body>
+    <a href="process.php">Process temporary entries</a>
+    <a href="admin/temporaryentries.php">See temporary entries</a>
     <table>
       <tr>
         <td>IP Address</td>
-        <td>Country</td>
-        <td>Flagged as</td>
+        <td>Hostname</td>
       <tr>
       <?php
         // BEGINNING OF SETTINGS
         $mysql_server = "mysql.server.org";
         $mysql_user = "iamthensa";
         $mysql_pass = "StalinDidN0th1ng";
+        $mysql_db = "ohshit";
         // ENDING OF SETTINGS
-    
-        function checkflags($row) {
-          if ($row['hasxxx'] = "1") {
-            $flags = "XXX";
-          } elseif ($row['hascp'] = "1") {
-            if ($flags) {
-              $flags = $flags + "+CP";
-            } else {
-              $flags = "CP";
-            }
-          } elseif ($row['isbot']) {
-            if ($flags) {
-              $flags = $flags + "+BOT";
-            } else {
-              $flags = "BOT";
-            }
-          }
-        }
         
-        $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass);
-        $sql = "SELECT ip, isp, country, hasxxx, hascp, isbot, date FROM trackrem_iplist";
+        $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass, $mysql_db);
+        $sql = "SELECT ip, hostname FROM trackrem_computerinfo";
         $result = $conn->query($sql);
     
         if ($result->numrows > 0 ) {
           while ($row=$results->fetch_assoc()) {
-            echo "<tr><td><a href='ipdata.php?id=" . $row['date'] . "'>" . $row['ip'] . "</a></td><td>" . $row['country'] . "</td><td>" . checkflags($row) . "</td></tr>";
+            echo "<tr><td><a href='admin/ipdata.php?id=" . $row['ip'] . "'>" . $row['ip'] . "</a></td><td>" . $row['hostname'] . "</td></tr>";
           }
         } else {
           echo "<tr><td>No input</td><td>No input</td><td>No input</td></tr>";
